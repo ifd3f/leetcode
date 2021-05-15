89f18380c616e5575ac068ba96a64891357ba9c9
Astrid Yu <astrid@astrid.tech>
2021-05-14 17:31:02 -0700

    Begin implementing blossom in earnest

--- distract_the_trainers.py
+++ distract_the_trainers.py
@@ -1,85 +1,205 @@
-def iterate(a, b):
-    if a == b:
-        return a, b
-
-    b -= a
-    a *= 2
-    if a > b:
-        a, b = b, a
-
-    return a, b
-
-
-def is_end(pair):
-    a, b = pair
-    return a == b
+from collections import defaultdict
 
 
 def is_infinite_cycle(a, b):
     """
     For 2 trainers with `a` and `b` bananas, do they undergo an infinite cycle?
     """
-    visited = set()
-    if a > b:
-        a, b = b, a
 
-    tortoise = a, b
-    hare = iterate(a, b)
-    while True:
-        if tortoise == hare:
-            return True
-        if is_end(tortoise) or is_end(hare):
-            return False
-        tortoise = iterate(*tortoise)
-        hare = iterate(*iterate(*hare))
+    # Is their sum a power of 2? (equivalent to the solution, as proved by modular arithmetic)
+    s = a + b
+
+    # Checking for power 2 https://stackoverflow.com/questions/57025836/how-to-check-if-a-given-number-is-a-power-of-two
+    return (s & (s - 1) == 0) and s != 0
 
 
-def edge(a, b):
-    if a > b:
-        return b, a
-    return a, b
+def mk_edge(a, b):
+    return (b, a) if a > b else (a, b)
 
 
-class NormalNode(object):
+class Forest(object):
     def __init__(self):
-        self.neighbors = []
+        self.parents = dict()
+        self.children = defaultdict(set)
+        self.node_to_is_even = dict()
 
-    def get_neighbors(self):
-        return self.neighbors
+    def __contains__(self, item):
+        return item in self.parents
+
+    def is_even(self, n):
+        return self.node_to_is_even[n]
+
+    def set_parent(self, x, p):
+        self.parents[x] = p
+        self.children[p].add(x)
+        if p is None:
+            self.node_to_is_even[x] = False
+        else:
+            self.node_to_is_even[x] = not self.node_to_is_even[p]
+
+    def get_children(self, x):
+        return self.children[x]
+
+    def get_parent(self, x):
+        return self.parents[x]
+
+    def get_root(self, x):
+        while self.get_parent(x) is not None:
+            x = self.get_parent(x)
+        return x
+
+    def get_path_to_root(self, x):
+        while self.get_parent(x) is not None:
+            yield x
+            x = self.get_parent(x)
 
 
-class ContractedBlossomNode(object):
-    def get_neighbors(self):
-        pass
+class Graph(object):
+    def __init__(self):
+        self.adjacency = defaultdict(set)
+        self.is_even = defaultdict(lambda: None)
+
+    def edges(self):
+        es = set()
+        for node, neighbors in self.adjacency.items():
+            for neighbor in neighbors:
+                edge = mk_edge(node, neighbor)
+                if edge not in es:
+                    yield edge
+                    es.add(edge)
+
+    def mate(self, n):
+        """If this graph represents a matching, returns the given node's matched mate, or None."""
+        neighbors = self.adjacency[n]
+        assert len(neighbors) <= 1, "Invalid matching: %s has more than 1 neighbor" % n
+        if len(neighbors) == 0:
+            return None
+        return next(iter(neighbors))  # take the only neighbor out
+
+    def add_edge_tup(self, e):
+        a, b = e
+        self.adjacency[a].add(b)
+        self.adjacency[b].add(a)
+
+    def rm_edge_tup(self, e):
+        a, b = e
+        self.adjacency[a].remove(b)
+        self.adjacency[b].remove(a)
+
+    def exposed_vertices(self, matching):
+        for node, neighbors in self.adjacency.items():
+            if matching.mate(node) is None:
+                yield node
+
+    def find_augmenting_path(self, matching):
+        # Resources:
+        # https://en.wikipedia.org/wiki/Blossom_algorithm#Finding_an_augmenting_path
+        # https://www.cs.tau.ac.il/~zwick/grad-algo-0910/match.pdf
+        forest = Forest()
+        marked_edges = set()
+
+        # Unmark all edges of matching
+        for edge in matching.edges():
+            marked_edges.add(edge)
+
+        # Use exposed nodes as roots of their own trees
+        exposed_nodes = list(self.exposed_vertices(matching))
+        for node in exposed_nodes:
+            # Add node to forest
+            forest.set_parent(node, None)
+
+        # Explore the graph from the exposed nodes.
+        for tree_root in exposed_nodes:
+            # Use a DFS because it's easiest.
+            search_stack = [tree_root]
+            visited = set()
+            while len(search_stack) > 0:
+                node = search_stack.pop()
+                if node in visited:
+                    continue
+                visited.add(node)
+
+                if node not in forest:
+                    x = matching.mate(node)
+                    pass
+
+                # If even, we can do stuff to it
+                if forest.is_even(node):
+                    mate = matching.mate(node)
+                    if mate is None:  # No mate, so this is unmatched and therefore exposed
+                        return list(forest.get_path_to_root(node))
+                    else:
+                        forest.set_parent(mate, node)  # Add the mate to the forest
+
+                    # Are they different trees?
+                    if forest.get_root(node) != tree_root:
+                        # Found an augmenting path
+                        path = [node]
+                        path.extend(forest.get_path_to_root(node))
+                        return path
+
+                    # Same tree: blossom detected.
+                    # TODO blossom code
+                    # graph2 = deepcopy(self)
+                    # matching2 = contract stuff
+                    # graph2.find_augmenting_path()
+                    assert False, "Blossom NYI"
+
+                # Add neighbors to the graph search
+                search_stack.extend(self.adjacency[node])
+
+        # No augmenting path has been found, so return a sentinel value.
+        return None
 
 
-# adjacency: List[Set[int]]
-# matching: Set[int]
+def augment(path, matching):
+    should_add = True
+    for i in range(len(path) - 1):
+        a = path[i]
+        b = path[i + 1]
 
-def contract(nodes, adjacency):
-    edges = {}
-    for node in nodes:
+        if should_add:
+            matching.add_edge_tup((a, b))
+        else:
+            matching.rm_edge_tup((a, b))
+
+        i += 1
+        should_add = not should_add
 
 
+class Contraction(object):
+    def __init__(self, graph, nodes, name):
+        self.graph = graph
+        self.contracted_nodes = nodes
+        self.name = name
+        self.removed_edges = None
+        self.neighbors = None
 
-def find_augmenting_path(adjacency, matching):
-    forest = []
-    unmarked_verts = set(range(len(adjacency)))
-    marked_edges = set(matching)
+    def __enter__(self):
+        self.removed_edges = []
+        self.neighbors = set()
+        for node in self.contracted_nodes:
+            for neighbor in self.graph.adjacency[node]:
+                self.removed_edges.append((node, neighbor))
+                self.neighbors.add(neighbor)
 
-    # Build set of exposed vertices
-    exposed = set(range(len(adjacency)))
-    for a, b in matching:
-        if a in exposed:
-            exposed.remove(a)
-        if b in exposed:
-            exposed.remove(b)
+        for edge in self.removed_edges:
+            self.graph.rm_edge_tup(edge)
 
-    for vertex in exposed:
-        forest.append([vertex])
+        for neighbor in self.neighbors:
+            self.graph.add_edge_tup((self.name, neighbor))
 
-    while len(unmarked_verts) > 0:
-        unmarked_verts
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_val is not None:
+            raise exc_val
+
+        for neighbor in self.neighbors:
+            self.graph.rm_edge_tup((self.name, neighbor))
+
+        for edge in self.removed_edges:
+            self.graph.add_edge_tup(edge)
 
 
 def find_maximum_matching(adjacency, matching):
@@ -88,18 +208,18 @@ def find_maximum_matching(adjacency, matching):
 
 
 def build_graph(xs):
-    i_to_adjacency = []
+    graph = Graph()
     for i, a in enumerate(xs):
-        adjacencies = set()
-        for j, b in enumerate(xs):
+        for j in range(i):
+            b = xs[j]
             if is_infinite_cycle(a, b):
-                adjacencies.add(j)
-        i_to_adjacency.append(adjacencies)
-    return i_to_adjacency
+                graph.add_edge_tup((i, j))
+    return graph
 
 
 def solution(bananas):
-    i_to_adjacency = build_graph(bananas)
+    graph = build_graph(bananas)
+    print(graph.adjacency)
 
     for i in range(len(bananas)):
         for j in range(i):
--- test_distract_the_trainers.py
+++ test_distract_the_trainers.py
@@ -1,6 +1,21 @@
 import unittest
 
-from distract_the_trainers import is_infinite_cycle, solution
+from distract_the_trainers import is_infinite_cycle, solution, Graph
+
+
+class TestGraph(unittest.TestCase):
+    def test_bipartite(self):
+        graph = Graph()
+        graph.add_edge_tup((1, 2))
+        graph.add_edge_tup((3, 4))
+        graph.add_edge_tup((1, 4))
+
+        matching = Graph()
+        matching.add_edge_tup((1, 4))
+
+        path = graph.find_augmenting_path(matching)
+
+        self.assertEqual([1, 2, 3, 4], path)
 
 
 class MyTestCase(unittest.TestCase):
