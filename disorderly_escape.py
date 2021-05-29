455ebc311379fa261c6256ba3ffd5aca6db7e12a
Astrid Yu <astrid@astrid.tech>
2021-05-29 14:21:10 -0700

    omg this is somehow working

--- disorderly_escape.py
+++ disorderly_escape.py
@@ -23,10 +23,13 @@ def solution(w, h, s):
     return str(f(w, h, s))
 
 
-class CIMonomial(object):
+class Mono(object):
     def __init__(self, factors):
-        self.factors = Counter(factors)
+        self.factors = factors
         self.hash = None
+
+    def __repr__(self):
+        return 'Mono(' + repr(dict(self.factors)) + ')'
 
     def __str__(self):
         return ' '.join('a_{%s}^{%s}' % (s, p) for s, p in self.factors.items())
@@ -37,78 +40,83 @@ class CIMonomial(object):
     def __hash__(self):
         if self.hash is None:
             self.hash = 0
-            for s, p in self.factors.items():
+            factors = list(self.factors)
+            factors.sort()
+            for b in factors:
+                p = self.factors[b]
                 self.hash <<= 3
-                self.hash += s * p
                 self.hash %= 2 ** 31
+                self.hash += b * p
         return self.hash
 
     def is_one(self):
         return all((p == 0 for _, p in self.factors.items()))
 
     def __mul__(self, other):
-        assert isinstance(other, CIMonomial), "Can only multiply monomials with monomials"
+        assert isinstance(other, Mono), "Can only multiply monomials with monomials"
         if other.is_one():
             return self
         if self.is_one():
             return other
-        return CIMonomial(self.factors + other.factors)
+        return Mono(Counter(self.factors) + Counter(other.factors))
 
     def cycle_cartesian(self, other):
-        assert isinstance(other, CIMonomial), "Can only multiply monomials with monomials"
+        assert isinstance(other, Mono), "Can only multiply monomials with monomials"
         if other.is_one() or self.is_one():
             return CIM_ONE
         result = Counter()
         for f1, p1 in self.factors.items():
             for f2, p2 in other.factors.items():
                 g = gcd(f1, f2)
+                a_pow = g * p1 * p2
                 a_sub = (f1 * f2) / g
-                a_pow = g
                 result[a_sub] += a_pow
-        return CIMonomial(result)
+        return Mono(result)
 
     def substitute(self, s):
         return prod((a ** p for a, p in self.factors.items()))
 
 
-CIM_ONE = CIMonomial({})
+CIM_ONE = Mono({})
 
 
-class CIPolynomial(object):
+class Poly(object):
     def __init__(self, monomials):
         self.monomials = monomials
 
     def __repr__(self):
-        return 'CIP(' + str(self) + ')'
+        return 'Poly(' + repr(dict(self.monomials)) + ')'
+
+    def __eq__(self, other):
+        return self.monomials == other.monomials
 
     @staticmethod
     def symmetric_group(n):
         symmetric_groups = [CIP_ZERO] * (n + 1)
-        symmetric_groups[0] = CIPolynomial({CIM_ONE: 1})
+        symmetric_groups[0] = Poly({CIM_ONE: 1})
         for m in range(1, n + 1):
             poly_sum = CIP_ZERO
             for i in range(1, m + 1):
-                mono = CIMonomial({i: 1})
-                poly = CIPolynomial({mono: 1})
+                mono = Mono({i: 1})
+                poly = Poly({mono: 1})
                 zs = symmetric_groups[m - i]
                 term = zs.multiply(poly)
                 poly_sum += term
             symmetric_groups[m] = poly_sum.scale(Fraction(1, m))
-            print symmetric_groups
         return symmetric_groups[n]
 
     def __str__(self):
         return ' + '.join('%s %s' % (c, m) for m, c in self.monomials.items())
 
     def multiply(self, other):
-        assert isinstance(other, CIPolynomial), "Can only multiply polynomials with polynomials"
+        assert isinstance(other, Poly), "Can only multiply polynomials with polynomials"
         result = defaultdict(lambda: Fraction(0))
         for m1, c1 in self.monomials.items():
             for m2, c2 in other.monomials.items():
                 monomial = m1 * m2
                 coeff = c1 * c2
                 result[monomial] += coeff
-        return CIPolynomial(result)
+        return Poly(result)
 
     def __add__(self, other):
         result = defaultdict(lambda: Fraction(0))
@@ -116,28 +124,28 @@ class CIPolynomial(object):
             result[m] += c
         for m, c in other.monomials.items():
             result[m] += c
-        return CIPolynomial(result)
+        return Poly(result)
 
     def scale(self, scalar):
-        return CIPolynomial({
+        return Poly({
             m1: c1 * scalar for m1, c1 in self.monomials.items()
         })
 
     def cycle_cartesian(self, other):
-        assert isinstance(other, CIPolynomial), "Can only multiply polynomials with polynomials"
+        assert isinstance(other, Poly), "Can only multiply polynomials with polynomials"
         result = defaultdict(lambda: Fraction(0))
         for m1, c1 in self.monomials.items():
             for m2, c2 in other.monomials.items():
                 monomial = m1.cycle_cartesian(m2)
                 coeff = c1 * c2
                 result[monomial] += coeff
-        return CIPolynomial(result)
+        return Poly(result)
 
     def substitute(self, s):
-        return prod((c ** m.substitute(s) for m, c in self.monomials.items()))
+        return prod((c * m.substitute(s) for m, c in self.monomials.items()))
 
 
-CIP_ZERO = CIPolynomial({})
+CIP_ZERO = Poly({})
 
 
 def grid_print(xss):
@@ -150,5 +158,8 @@ def grid_print(xss):
 
 
 if __name__ == '__main__':
-    print CIPolynomial.symmetric_group(2)
-    print CIPolynomial.symmetric_group(3)
+    p1 = Poly.symmetric_group(2)
+    p2 = Poly.symmetric_group(3)
+    print repr(p1.cycle_cartesian(p1))
+
+    print p1.cycle_cartesian(p2).substitute(2)
--- test_disorderly_escape.py
+++ test_disorderly_escape.py
@@ -3,10 +3,6 @@ import unittest
 from disorderly_escape import *
 
 
-def symp(n, k, s):
-    return SymmetricMonomialProduct(n, k, s)(n, k)
-
-
 def symmetric_orbits(n, s):
     if n == 0:
         return 1
@@ -16,45 +12,60 @@ def symmetric_orbits(n, s):
     return total / n
 
 
-class TestHelpers(unittest.TestCase):
-    def test_symp_0(self):
-        self.assertEqual(6, symp(1, 9, 6))
+class TestMonomial(unittest.TestCase):
+    def test_monomial_hashes_correctly(self):
+        m1 = Mono({5: 3, 3: 2, 6: 5})
+        m2 = Mono(Counter({3: 2, 6: 5, 5: 3}))
 
-    def test_symp_1(self):
-        self.assertEqual(36, symp(2, 2, 6))
+        h1, h2 = hash(m1), hash(m2)
 
-    def test_symp_2(self):
-        self.assertEqual(9, symp(2, 2, 3))
+        self.assertEqual(h1, h2)
 
-    def test_symp_3(self):
-        self.assertEqual(10, symp(2, 3, 4))
+    def test_cycle_cartesian_1(self):
+        m1 = Mono({3: 1})
+        m2 = Mono({6: 1})
+        actual = Mono({6: 3})
 
-    def test_symp_4(self):
-        self.assertEqual(1, symp(4, 0, 4))
+        prod = m1.cycle_cartesian(m2)
 
-    def test_grid_0(self):
-        self.assertEqual(1, f(0, 3, 3))
+        self.assertEqual(actual, prod)
 
-    def test_grid_1(self):
-        self.assertEqual(10, f(1, 3, 3))
 
-    def test_grid_2(self):
-        self.assertEqual(13, f(2, 3, 2))
+class TestPolynomial(unittest.TestCase):
+    def test_symmetric_group_2(self):
+        p1 = Poly.symmetric_group(2)
 
-    def test_grid_3(self):
-        self.assertEqual(430, f(2, 3, 4))
+        actual = Poly({Mono({1: 2}): Fraction(1, 2), Mono({2: 1}): Fraction(1, 2)})
+        self.assertEqual(actual, p1)
 
-    def test_grid_4(self):
-        self.assertEqual(10, f(1, 3, 3))
+    def test_symmetric_group_3(self):
+        p1 = Poly.symmetric_group(3)
 
-    def test_grid_5(self):
-        self.assertEqual(3, f(1, 2, 2))
+        actual = Poly({Mono({3: 1}): Fraction(1, 3), Mono({1: 1, 2: 1}): Fraction(1, 2), Mono({1: 3}): Fraction(1, 6)})
+        self.assertEqual(actual, p1)
 
-    def test_grid_6(self):
-        self.assertEqual(4, f(1, 3, 2))
+    def test_cycle_cartesian_1(self):
+        p1 = Poly.symmetric_group(2)
 
-    def test_grid_7(self):
-        self.assertEqual(symmetric_orbits(8, 4), f(1, 8, 4))
+        prod = p1.cycle_cartesian(p1)
+
+        actual = Poly({Mono({1: 4}): Fraction(1, 4), Mono({2: 2}): Fraction(3, 4)})
+        self.assertEqual(actual, prod)
+
+    def test_cycle_cartesian_2(self):
+        p1 = Poly.symmetric_group(2)
+        p2 = Poly.symmetric_group(3)
+
+        prod = p1.cycle_cartesian(p2)
+
+        actual = Poly({
+            Mono({1: 6}): Fraction(1, 12),
+            Mono({2: 3}): Fraction(1, 3),
+            Mono({1: 2, 2: 2}): Fraction(1, 4),
+            Mono({3: 2}): Fraction(1, 6),
+            Mono({6: 1}): Fraction(1, 6),
+        })
+        self.assertEqual(actual, prod)
 
 
 class TestSolution(unittest.TestCase):
