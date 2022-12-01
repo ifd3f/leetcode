1446202548e03805c964bbe2114427560f7d5e18
Astrid Yu <astrid@astrid.tech>
2022-11-30 21:05:21 -0800

    add nix shit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in 
+      with pkgs; mkShell {
+        buildInputs = [
+          python3
+          racket
+        ];
+      };
+
+  };
+}
