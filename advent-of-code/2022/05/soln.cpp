f7105e40cb8944f6dec8c286573d23b8e35079f4
Astrid Yu <astrid@astrid.tech>
2022-12-05 01:06:46 -0800

    aoc2022: Get day 5 part 1 working!!!!!!

--- advent-of-code/2022/05/soln.cpp
+++ advent-of-code/2022/05/soln.cpp
@@ -16,8 +16,33 @@ struct get_top_items<cons<x, xs>> {
     using value = cons<typename x::car, typename get_top_items<xs>::value>;
 };
 
+template <>
+struct get_top_items<nil> {
+    using value = nil;
+};
+
+template <typename xs>
+struct print_charlist;
+
+template <char c, typename xs>
+struct print_charlist<cons<charbox<c>, xs>> {
+    static inline void run() {
+        std::cout << c;
+        print_charlist<xs>::run();
+    };
+};
+
+template <>
+struct print_charlist<nil> {
+    static inline void run() { };
+};
+
 int main() {
-    std::cout << typeid(result).name();
+    std::cout << "Part 1: ";
+    print_charlist<typename get_top_items<result>::value>::run();
+    std::cout << std::endl;
+
+    // std::cout << "Part 2: ";
     return 0;
 }
 
