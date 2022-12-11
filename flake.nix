579a38220b4020a4d228c584996dda75cc242b3f
Astrid Yu <astrid@astrid.tech>
2022-12-10 19:52:47 -0800

    aoc2022: Initialize day 6

--- flake.nix
+++ flake.nix
@@ -2,15 +2,12 @@
   description = "A very basic flake";
 
   outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in 
-      with pkgs; mkShell {
-        nativeBuildInputs = [
-          python3
-          racket
-          sqlite
-          clojure
-          perl
-        ];
+    devShells.x86_64-linux.default =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in with pkgs;
+      mkShell {
+        nativeBuildInputs = [ clojure perl python3 racket sqlite ]
+          ++ (with ocamlPackages; [ dune_3 findlib ocaml opam ]);
       };
   };
 }
